ee7e8901a16f5f50df8eadf637abbd2de7d9e5ac
Saisha Lee <saisha_alee@yahoo,com>
2025-12-03 05:17:06 -0600

    Trying to see if it works now

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from .models import Blogger, BlogPost, Comment
 from .forms import CommentForm
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 
 # Create your views here.
 
