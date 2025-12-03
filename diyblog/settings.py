16604ba8bcd8967ad5a91f30e9414501f80680af
Saisha Lee <saisha_alee@yahoo,com>
2025-12-03 04:38:51 -0600

    trying to see

--- diyblog/settings.py
+++ diyblog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
 
 ALLOWED_HOSTS = ['miniblogofsaisha-e39363f19efc.herokuapp.com']
 
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'blog' / 'static',
 ]
